3b81e764ac24d52ae89178756bca6c63f8412720
motyliar <j.turostowski@gmail.com>
2024-04-17 21:16:54 +0200

    creating bloc provider to getTasks

--- lib/presentation/app/view/app.dart
+++ lib/presentation/app/view/app.dart
@@ -2,19 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:task_app/core/constans/export.dart';
 import 'package:task_app/core/router/app_router.dart';
+import 'package:task_app/core/services/get_it/task_container.dart';
+import 'package:task_app/presentation/app/business/cubit/get_tasks_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      onGenerateRoute: AppRouter.onGenerateRoute,
-      initialRoute: kDashBoardPage,
-      debugShowCheckedModeBanner: false,
-      supportedLocales: AppLocalizations.supportedLocales,
-      locale: Locale('en'),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
+    return BlocProvider(
+      create: (context) => taskLocator<GetTasksCubit>()..fetchTasks(),
+      child: const MaterialApp(
+        onGenerateRoute: AppRouter.onGenerateRoute,
+        initialRoute: kDashBoardPage,
+        debugShowCheckedModeBanner: false,
+        supportedLocales: AppLocalizations.supportedLocales,
+        locale: Locale('en'),
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+      ),
     );
   }
 }
