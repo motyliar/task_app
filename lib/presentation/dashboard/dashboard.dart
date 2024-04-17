e252ee8229d35a19a873012415f6a5fde685d20c
motyliar <j.turostowski@gmail.com>
2024-04-17 22:32:31 +0200

    refactoring dashboard page

--- lib/presentation/dashboard/dashboard.dart
+++ lib/presentation/dashboard/dashboard.dart
@@ -1,22 +1,12 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:gap/gap.dart';
 import 'package:task_app/core/constans/export.dart';
 import 'package:task_app/core/l10n/l10n.dart';
-import 'package:task_app/core/params/task_params.dart';
 import 'package:task_app/core/router/route_animation.dart';
-import 'package:task_app/core/services/get_it/task_container.dart';
 import 'package:task_app/core/themes/colors.dart';
-import 'package:task_app/core/themes/text_style.dart';
-import 'package:task_app/domain/entity/task_entity.dart';
-import 'package:task_app/domain/subentity/data_marker.dart';
-import 'package:task_app/domain/subentity/task_status.dart';
-import 'package:task_app/presentation/dashboard/business/cubit/tasks_handler/tasks_handler_cubit.dart';
-import 'package:task_app/presentation/dashboard/business/logic/dashboard_helpers.dart';
+import 'package:task_app/presentation/app/business/cubit/get_tasks_cubit.dart';
 import 'package:task_app/presentation/dashboard/business/switch_button.dart';
+import 'package:task_app/presentation/dashboard/widgets/add_task_bottom_sheet.dart';
 import 'package:task_app/presentation/dashboard/widgets/custom_sliver_appbar.dart';
 import 'package:task_app/presentation/dashboard/widgets/main_label_text.dart';
 import 'package:task_app/presentation/dashboard/widgets/single_task.dart';
@@ -56,12 +46,16 @@ class Dashboard extends StatelessWidget {
                 ),
               ),
               SliverToBoxAdapter(
-                child: Column(
-                  children: List.generate(
-                      10,
-                      (index) => SingleTask(
-                            task: singleEntity,
-                          )),
+                child: BlocBuilder<GetTasksCubit, GetTasksState>(
+                  builder: (context, state) {
+                    return Column(
+                      children: List.generate(
+                          state.tasks.length,
+                          (index) => SingleTask(
+                                task: state.tasks[index],
+                              )),
+                    );
+                  },
                 ),
               ),
               const SliverToBoxAdapter(
@@ -76,101 +70,8 @@ class Dashboard extends StatelessWidget {
           foregroundColor: AppColors.accent,
           backgroundColor: AppColors.accent,
           onPressed: () {
-            showModalBottomSheet(
-                context: context,
-                builder: (context) => MultiBlocProvider(
-                      providers: [
-                        BlocProvider(
-                          create: (context) => SwitchButtonCubit(),
-                        ),
-                        BlocProvider(
-                          create: (context) => taskLocator<TasksHandlerCubit>(),
-                        ),
-                      ],
-                      child: Form(
-                          child: Padding(
-                        padding:
-                            const EdgeInsets.only(left: 30, right: 10, top: 20),
-                        child: Column(
-                          children: [
-                            TextFormField(
-                              controller: _titleController,
-                              maxLength: 50,
-                              decoration:
-                                  const InputDecoration(hintText: 'title'),
-                            ),
-                            TextFormField(
-                              controller: _descriptionController,
-                              maxLength: 500,
-                              minLines: 4,
-                              maxLines: 10,
-                              decoration: const InputDecoration(
-                                  hintText: 'description'),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                SizedBox(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.5,
-                                  child: TextFormField(
-                                    controller: _ownerController,
-                                    maxLength: 20,
-                                    decoration: const InputDecoration(
-                                        hintText: 'owner'),
-                                  ),
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  children: [
-                                    const Text("Is priority?"),
-                                    const Gap(5),
-                                    BlocBuilder<SwitchButtonCubit, bool>(
-                                      builder: (context, state) {
-                                        return GestureDetector(
-                                          onTap: () => context
-                                              .read<SwitchButtonCubit>()
-                                              .changePriority(),
-                                          child: CheckBox(
-                                            visible: state,
-                                          ),
-                                        );
-                                      },
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            const Gap(10),
-                            Align(
-                              alignment: Alignment.bottomRight,
-                              child: TextButton(
-                                  onPressed: () async {
-                                    await DashboardHelpers.pickDeadlineDate(
-                                            context)
-                                        .then((response) => print(response));
-                                  },
-                                  child: const Text("choose date")),
-                            ),
-                            BlocBuilder<TasksHandlerCubit, TasksHandlerState>(
-                              builder: (context, state) {
-                                return ElevatedButton(
-                                    onPressed: () => context
-                                        .read<TasksHandlerCubit>()
-                                        .addTask(TaskParams(
-                                            title: _titleController.text,
-                                            description:
-                                                _descriptionController.text,
-                                            deadline: DateTime.now(),
-                                            owner: _ownerController.text)),
-                                    child: const Text("ADD"));
-                              },
-                            ),
-                          ],
-                        ),
-                      )),
-                    ));
+            AddTaskBottomSheet(context, _titleController,
+                _descriptionController, _ownerController);
           },
           child: const Icon(
             Icons.add,
@@ -181,37 +82,3 @@ class Dashboard extends StatelessWidget {
     );
   }
 }
-
-class CheckBox extends StatelessWidget {
-  const CheckBox({super.key, required this.visible});
-
-  final bool visible;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 22,
-      height: 22,
-      color: Colors.grey.withOpacity(0.5),
-      child: visible
-          ? Container(
-              margin: const EdgeInsets.all(6),
-              decoration: BoxDecoration(
-                  color: Colors.red, borderRadius: BorderRadius.circular(50)),
-            )
-          : const SizedBox(),
-    );
-  }
-}
-
-TaskEntity singleEntity = TaskEntity(
-    id: '11',
-    title: "Umyć podłogę",
-    description: "Dzisiaj należy umyć podłogę i pozmywać naczynia",
-    deadline: DateTime.now(),
-    owner: "Wacek",
-    status: TaskStatus.planned,
-    stat: DateStat(
-        create: DateMarker.currentDay(),
-        execution: DateMarker(),
-        done: DateMarker()));
