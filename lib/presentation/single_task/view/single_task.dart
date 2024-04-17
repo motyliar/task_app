3c49daaaa99bc46a88e86b6a07662e060cd0b044
motyliar <j.turostowski@gmail.com>
2024-04-18 00:32:34 +0200

    create single task page and update router

---
+++ lib/presentation/single_task/view/single_task.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:gap/gap.dart';
+import 'package:task_app/core/constans/export.dart';
+import 'package:task_app/core/l10n/l10n.dart';
+import 'package:task_app/core/router/route_animation.dart';
+import 'package:task_app/core/themes/colors.dart';
+import 'package:task_app/core/themes/text_style.dart';
+import 'package:task_app/core/utils/extensions.dart';
+import 'package:task_app/domain/entity/task_entity.dart';
+import 'package:task_app/presentation/dashboard/business/logic/dashboard_helpers.dart';
+
+class SingleTaskPage extends StatelessWidget {
+  const SingleTaskPage({required this.task, super.key});
+
+  final TaskEntity task;
+
+  static Route route(TaskEntity taskEntity) {
+    return RouteAnimations.slidingDownRoute(
+        child: SingleTaskPage(task: taskEntity), route: kSingleTask);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return Scaffold(
+      backgroundColor: AppColors.scaffoldBackground,
+      appBar: AppBar(
+        backgroundColor: AppColors.scaffoldBackground,
+        title: Text(
+          l10n.singleTask,
+          style: AppTextStyles.headersMedium,
+        ),
+      ),
+      body: SafeArea(
+          child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Divider(),
+            const Gap(5),
+            Text("${l10n.deadline} ${task.deadline.toString().cut(10)}"),
+            const Gap(20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  task.title,
+                  style: AppTextStyles.headersMedium
+                      .copyWith(fontWeight: FontWeight.bold),
+                ),
+                Text(DashboardHelpers.convertStatusToLocalizationName(
+                    task.status, context)),
+              ],
+            ),
+            const Gap(20),
+            Text(
+              task.description,
+              style: AppTextStyles.descriptionMid,
+            ),
+            const Gap(5),
+            Align(
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  task.owner.toUpperCase(),
+                  style: AppTextStyles.descriptionMid,
+                )),
+            const Divider(),
+            const Gap(10),
+            Text(
+              l10n.changeStatus,
+              style: AppTextStyles.descriptionBig
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                    onPressed: () {}, child: Text(l10n.planned.toUpperCase())),
+                ElevatedButton(
+                    onPressed: () {},
+                    child: Text(l10n.executing.toUpperCase())),
+                ElevatedButton(
+                    onPressed: () {}, child: Text(l10n.done.toUpperCase()))
+              ],
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
