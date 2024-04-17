d1e87fa25a992e309459111cc401025e67f8cdfb
motyliar <j.turostowski@gmail.com>
2024-04-17 11:47:53 +0200

    creating error page class

--- lib/presentation/error/view/error_page.dart
+++ lib/presentation/error/view/error_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:task_app/core/constans/export.dart';
 import 'package:task_app/core/router/route_animation.dart';
 
 class ErrorPage extends StatelessWidget {
   const ErrorPage({super.key});
 
   static Route route() {
-    return RouteAnimations.slidingDownRoute(child: const ErrorPage(), route: _kErrorPage)
-  } 
+    return RouteAnimations.slidingDownRoute(
+        child: const ErrorPage(), route: kErrorPage);
+  }
 
   @override
   Widget build(BuildContext context) {
