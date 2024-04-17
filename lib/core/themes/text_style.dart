f5487abe4203177c72a3a906fcafcb02b66e3049
motyliar <j.turostowski@gmail.com>
2024-04-17 21:15:46 +0200

    creating themes - colors, textStyle

---
+++ lib/core/themes/text_style.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:task_app/core/themes/colors.dart';
+
+class AppTextStyles {
+  static const TextStyle headersMedium = TextStyle(
+    color: AppColors.accent,
+    fontSize: 22,
+  );
+  static const TextStyle headersBig = TextStyle(
+    color: AppColors.accent,
+    fontSize: 30,
+  );
+
+  static const TextStyle descriptionBig = TextStyle(
+    color: AppColors.accent,
+    fontSize: 18,
+  );
+
+  static const TextStyle descriptionMid = TextStyle(
+    color: AppColors.accent,
+    fontSize: 16,
+  );
+
+  static const TextStyle descriptionSmall = TextStyle(
+    color: AppColors.accent,
+    fontSize: 14,
+  );
+}
