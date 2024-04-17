2b4f043a3385799bf68fbc2fa2e0414a76973321
motyliar <j.turostowski@gmail.com>
2024-04-17 14:16:27 +0200

    creating repository override function to handling addTask

--- lib/domain/usecases/add_task_usecase.dart
+++ lib/domain/usecases/add_task_usecase.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:task_app/core/params/task_params.dart';
 import 'package:task_app/domain/helpers/usecase_status.dart';
 import 'package:task_app/domain/repository/task_repository.dart';
 
@@ -7,7 +8,7 @@ class AddTaskUseCase {
       : _repository = repository;
   final TaskRepository _repository;
 
-  Either<Exception, UseCaseStatus> execute() {
-    return _repository.addTask();
+  Future<Either<Exception, UseCaseStatus>> execute(TaskParams params) {
+    return _repository.addTask(params);
   }
 }
