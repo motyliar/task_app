c5ccf06377706d7ae0ea7a9550d251eaf800a388
motyliar <j.turostowski@gmail.com>
2024-04-17 14:57:39 +0200

    create and testing convertStatus

--- lib/domain/subentity/task_status.dart
+++ lib/domain/subentity/task_status.dart
@@ -1,1 +1,14 @@
 enum TaskStatus { planned, executing, done }
+
+TaskStatus convertStatus(String name) {
+  switch (name) {
+    case "planned":
+      return TaskStatus.planned;
+    case "executing":
+      return TaskStatus.executing;
+    case "done":
+      return TaskStatus.done;
+    default:
+      return TaskStatus.planned;
+  }
+}
