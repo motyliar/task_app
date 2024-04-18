df8d4ff21b153d87da133b0e735b6ec9850e90b4
motyliar <j.turostowski@gmail.com>
2024-04-18 13:35:06 +0200

    create weather params

--- lib/core/params/weather_params.dart
+++ lib/core/params/weather_params.dart
@@ -1,3 +1,10 @@
+import 'package:task_app/core/constans/weather_api.dart';
 import 'package:task_app/core/params/i_params.dart';
 
-class WeatherParams extends IParams {}
+class WeatherParams extends IParams {
+  const WeatherParams({required this.lat, required this.lng});
+  final String lng;
+  final String lat;
+
+  Uri getUrl() => Uri.parse(WeatherAPI().api(lat, lng));
+}
