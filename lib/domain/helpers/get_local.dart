add75f2e1694af9eff58a87dc96a2f7e34c4ac2a
motyliar <j.turostowski@gmail.com>
2024-04-18 13:36:23 +0200

    create geolocalization class

--- lib/domain/helpers/get_local.dart
+++ lib/domain/helpers/get_local.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:task_app/core/params/weather_params.dart';
 
 class GetLocalization {
   Future<Position> geoLocation() async {
@@ -10,5 +11,12 @@ class GetLocalization {
         desiredAccuracy: LocationAccuracy.low);
     debugPrint(position.toString());
     return position;
+  }
+
+  Future<WeatherParams> getParams() async {
+    final position = await geoLocation();
+    final lat = position.latitude.toStringAsFixed(2);
+    final lng = position.longitude.toStringAsFixed(2);
+    return WeatherParams(lat: lat, lng: lng);
   }
 }
