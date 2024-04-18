9a4ca8146fbee37e7667bde570c189958202977d
motyliar <j.turostowski@gmail.com>
2024-04-18 13:37:28 +0200

    create get weather usecase

---
+++ lib/domain/usecases/get_current_weather.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:task_app/core/params/weather_params.dart';
+import 'package:task_app/domain/entity/weather_entity.dart';
+import 'package:task_app/domain/repository/weather_repository.dart';
+
+class GetCurrentWeatherUsecase {
+  const GetCurrentWeatherUsecase({required WeatherRepository repository})
+      : _repository = repository;
+
+  final WeatherRepository _repository;
+
+  Future<Either<Exception, WeatherEntity>> execute(WeatherParams params) {
+    return _repository.getCurrentWeather(params);
+  }
+}
